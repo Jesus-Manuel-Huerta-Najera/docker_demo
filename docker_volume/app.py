e7d5be85efb228a5991a0d52c7ab63e1fc0979b3
Jesus-Manuel-Huerta-Najera <1718110393@utectulancingo.edu.mx>
2021-05-22 01:52:58 +0000

    commit on master

--- docker_volume/app.py
+++ docker_volume/app.py
@@ -3,6 +3,7 @@ import web
 urls = (
     '/', 'mvc.controller.index.Index',
     '/ubuntu', 'mvc.controller.ubuntu.Ubuntu',
+    '/docker', 'mvc.controller.docker.Docker',
   
 )
 app = web.application(urls, globals())
--- docker_volume/mvc/controller/docker.py
+++ docker_volume/mvc/controller/docker.py
@@ -2,10 +2,10 @@ import web
 
 render = web.template.render("mvc/view/")
 
-class Index():
+class Docker():
 
     def GET(self):
         try:
-            return render.index() # renderizando formulario.html
+            return render.docker() # renderizando formulario.html
         except Exception as e:
             return "Error " + str(e.args)
