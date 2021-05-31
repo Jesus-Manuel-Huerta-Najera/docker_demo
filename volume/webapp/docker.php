a1e78c916ff62f5c3c314381ab9b7fb71c4ff048
Jesus-Manuel-Huerta-Najera <1718110393@utectulancingo.edu.mx>
2021-05-31 03:15:34 +0000

    commit

---
+++ volume/webapp/docker.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Docker</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+  </head>
+  <body>
+      <!-- NAVEGACIÓN -->
+      <nav class="navbar is-black" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-brand">
+                  <a class="navbar-item" href="https://docs.microsoft.com/es-es/dotnet/architecture/microservices/container-docker-introduction/docker-defined">
+                      Docker
+                  </a> 
+                </div>
+                <div class="navbar-end">
+
+                    <a href="https://github.com/Jesus-Manuel-Huerta-Najera/" class="navbar-item">
+                        Jesus Manuel Huerta Najera
+                    </a>
+                    <a href="./index.php" class="navbar-item">
+                      regresar
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav><!-- NAVEGACIÓN -->
+    <!-- HERO -->
+    <div class="has-background-grey-lighter">
+      <div class="container my-5 px-3">
+          <section class="columns">
+              <div class="column is-one-third">
+                  <img src="https://th.bing.com/th/id/OIP.yFk3VAJ8hYPNO2z5eI_MHgHaEc?pid=ImgDet&rs=1" alt="">
+              </div>
+              <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                  <div>
+                      <p>
+                        Docker es un proyecto que es de código abierto para poder automatizar la implementación de aplicaciones como contenedores portátiles y así mismo que sean autosuficientes que se pueden ejecutar en la nube o localmente, por eso docker es también una empresa que puede proveer e impulsar esta tecnología, está en colaboración de la nube, Linux y Windows. (Microsoft, 2018) 
+                      </p>
+                  </div>
+                  
+              </div>
+          </section>
+      </div>
+  </div><!-- HERO -->
+  <!-- HERO -->
+  <div class="has-background-grey-lighter">
+    <div class="container my-5 px-3">
+        <section class="columns">
+           
+            <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                <div>
+                    <h3 class="is-size-3 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
+                        ¿Dónde se ejecutan?
+                    </h3>
+                    <p>
+                        Los contenedores de docker se pueden ejecutar en cualquier lugar, puede ser a nivel local en el centro de datos de cliente, en un proveedor de servicios externo, incluso debería ejecutar de forma natica en Linux y Windows. Las imágenes de Windows solo pueden ejecutarse en un host de Windows y las imágenes de Linux, hacen lo mismo al solo poder trabajar en un host con el mismo sistema. (Microsoft, 2018)
+                    </p>
+                    <br>
+                    <p>
+                        Los programadores suelen ejecutar los entornos de desarrollo de Windows, Linux o macos, cada equipo de desarrollo ejecuta un host de docker en el que se implementa imágenes de docker, estas incluyen aplicaciones y dependencias para facilitar la ejecución de código y desarrollo del mismo. (Microsoft, 2018)                        
+
+                    </p>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+</div><!-- HERO -->
+  </body>
+</html>
---
+++ volume/webapp/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contenedor</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+  </head>
+  <body>
+      <!-- NAVEGACIÓN -->
+      <nav class="navbar is-black" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-brand">
+                  <a class="navbar-item" href="https://www.hostgator.mx/blog/containers-que-son-en-programacion/">
+                      Contenedor
+                  </a> 
+                </div>
+                <div class="navbar-end">
+
+                    <a href="https://github.com/Jesus-Manuel-Huerta-Najera/" class="navbar-item">
+                        Jesus Manuel Huerta Najera
+                    </a>
+                    <a href="./ubuntu.php" class="navbar-item">
+                      Ubuntu
+                    </a>
+
+                    <a href="./docker.php" class="navbar-item">
+                      Docker
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav><!-- NAVEGACIÓN -->
+    <!-- HERO -->
+    <div class="has-background-grey-lighter">
+      <div class="container my-5 px-3">
+          <section class="columns">
+              <div class="column is-one-third">
+                  <img src="https://th.bing.com/th/id/OIP.lpQP2YssYJOrUi6tsxzOswAAAA?pid=ImgDet&rs=1" alt="">
+              </div>
+              <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                  <div>
+                      <p>
+                        Un contenedor podría ser imaginado como una caja, una caja gigante que los barcos suelen cargar, sin embargo, en el mundo de la programación esta palabra tiene un sentido totalmente distinto. Los contenedores son usados dentro del desarrollo, puede ser utilizado para pruebas y también para facilitar la producción de software, pues los contenedores permiten ejecutar múltiples sistemas aislados dentro de un sistema en operación real. (Host México, 2018) 
+                      </p>
+                  </div>
+                  
+              </div>
+          </section>
+      </div>
+  </div><!-- HERO -->
+  <!-- HERO -->
+  <div class="has-background-grey-lighter">
+    <div class="container my-5 px-3">
+        <section class="columns">
+           
+            <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                <div>
+                    <h3 class="is-size-3 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
+                      ¿Que es un contenedor?
+                    </h3>
+                    <p>
+                      Los contenedores son ambientes aislados dentro de un servidor web, que comparte un único host que desarrolla el roll de host, para entenderlo mejor podría usarse como ejemplo un barco de carga que puede transportar diversos contenedores, si una de las unidades se llegara a averiar, los demás contenedores no serían afectados porque  estarían protegidos de forma individual y cargan sus propios productos, que en programación sería un sistema de control donde contiene o guarda contenedores con distintos sistemas en cada uno y los productos son herramientas para listos para usar. (Host México, 2018)
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</div><!-- HERO -->
+ <!-- HERO -->
+ <div class="has-background-grey-lighter">
+  <div class="container my-5 px-3">
+      <section class="columns">
+         
+          <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+              <div>
+                <h3 class="is-size-3 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
+                  ¿Cuál es la diferencia de un contenedor y una virtualización?
+                </h3>
+                <p>
+                  En una máquina virtual es posible utilizar distintos recursos, pero todo se ejecuta dentro del mismo sistema operacional, si un problema ocurre, todas las funciones serán afectadas. Cuando en el caso de un contenedor la idea es que cada uno realice una función o responsabilidad, es decir que uno podría ejecutar apache y el otro php, de esta forma es posible aislar los procesos de cada herramienta, esto garantiza que ninguna de ellas comprometa el funcionamiento de la otra. (Host México, 2018)
+                </p>
+            </div>
+          </div>
+      </section>
+  </div>
+</div><!-- HERO -->  
+</body>
+</html>
