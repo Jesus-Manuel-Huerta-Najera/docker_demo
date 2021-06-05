41615b858a880161a5adab690981c011bdfa2689
Jesus-Manuel-Huerta-Najera <1718110393@utectulancingo.edu.mx>
2021-06-05 03:55:07 +0000

    commit on git@github.com-Jesus-Manuel-Huerta-Najera/docker_demo.git

---
+++ volumen_commit/webbapp/ubuntu.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ubuntu</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+  </head>
+  <body>
+      <!-- NAVEGACIÓN -->
+      <nav class="navbar is-black" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-brand">
+                  <a class="navbar-item" href="https://www.hostingplus.pe/blog/ubuntu-que-es-caracteristicas-y-como-funciona/#:%7E:text=Las%20principales%20caracter%C3%ADsticas%20de%20Ubuntu%20son%3A%201%20Vers%C3%A1til.,Nvidia.%20. . .%204%20Arranque%20y%20apagado%20veloz.%20">
+                      Ubuntu
+                  </a> 
+                </div>
+                <div class="navbar-end">
+
+                    <a href="https://github.com/Jesus-Manuel-Huerta-Najera/" class="navbar-item">
+                        Jesus Manuel Huerta Najera
+                    </a>
+                    <a href="./index.php" class="navbar-item">
+                      Regresar
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav><!-- NAVEGACIÓN -->
+    <!-- HERO -->
+    <div class="has-background-grey-lighter">
+      <div class="container my-5 px-3">
+          <section class="columns">
+              <div class="column is-one-third">
+                  <img src="https://th.bing.com/th/id/Red85858c521113f0ceca45d7d287f369?rik=Gl9vZPY74ThwMg&pid=ImgRaw" alt="">
+              </div>
+              <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                  <div>
+                      <p>
+                        Muchas distribuciones de Linux como es Ubuntu, han intentado poder reducir la distancia del sistema operativo de código libre con los usuarios, para que pueda tener interfaces graficas que puedan realizar la mayor parte de tareas de sistema operativo de forma fácil e intuitiva, para poder evitar el recurrir a la escritura de comandos y parámetros. (HostingPlus, 2020) 
+                      </p>
+                  </div>
+                  
+              </div>
+          </section>
+      </div>
+  </div><!-- HERO -->
+  <!-- HERO -->
+  <div class="has-background-grey-lighter">
+    <div class="container my-5 px-3">
+        <section class="columns">
+            <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                <div>
+                    <h3 class="is-size-3 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
+                        ¿Qué es Ubuntu?
+                    </h3>
+                    <p>
+                        Es una distribución de Linux que se ha convertido en el más popular debido a su facilidad de uso y también a su gran rendimiento en equipos y servidores. (HostingPlus, 2020)
+                    </p>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+</div><!-- HERO -->
+  <!-- HERO -->
+  <div class="has-background-grey-lighter">
+    <div class="container my-5 px-3">
+        <section class="columns">
+            <div class="column is-two-thirds is-flex is-flex-direction-column is-justify-content-space-between">
+                <div>
+                    <h3 class="is-size-3 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
+                        ¿Cuáles son sus características?
+                    </h3>
+                    <p>
+                        •	Versátil. Puede instalarse en equipos de sobremesa, ordenadores portátiles o en servidores (para gestionar almacenamiento web, por ejemplo). 
+                    </p>
+                    <br>
+                    <p>
+                        •	Diferentes entornos de escritorio. La apariencia de Ubuntu puede variarse haciendo uso de una serie de entornos de escritorio. Cada uno proporciona una serie de herramientas como la administración de archivos, visores de imágenes y documentos PDF, y similares.
+                    </p>
+                    <br>
+                    <p>
+                        •	Incluye controladores Nvidia. Ubuntu incorpora controladores para el principal fabricante de tarjetas gráficas, lo que aumenta el rendimiento de los equipos donde se instala, a la vez que reduce el tiempo de configuración del sistema tras su instalación.
+                    </p>
+                    <br>
+                    <p>
+                        •	Arranque y apagado veloz. Ubuntu es un sistema que se caracteriza por iniciarse o apagarse en muy poco tiempo.
+                        (HostingPlus, 2020)                        
+                    </p>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+</div><!-- HERO -->
+  </body>
+</html>
